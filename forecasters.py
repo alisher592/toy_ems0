90bae77d6ea3ef81198607aaae0408cb3fb0ac6f
alisher592 <53061266+alisher592@users.noreply.github.com>
2021-10-28 16:53:59 +0300

    Dummy Load forecaster added

---
+++ forecasters.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import median_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import max_error
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from datetime import datetime, timedelta
+
+class LoadForecaster:
+
+    def __init__(self, files_destination):
+        self.model = pickle.load(open(files_destination[0], 'rb'))  # загрузка модели из pickle-файла
+
+        self.X_scaler = MinMaxScaler()
+        self.Y_scaler = MinMaxScaler()
+
+        self.X_scaler.min_ = pickle.load(open(files_destination[1], 'rb'))[0]
+        self.X_scaler.scale_ = pickle.load(open(files_destination[1], 'rb'))[1]
+
+        self.Y_scaler.min_ = pickle.load(open(files_destination[2], 'rb'))[0]
+        self.Y_scaler.scale_ = pickle.load(open(files_destination[2], 'rb'))[1]
+
+        self.current_datetime = datetime.now()
+
+    def hour_of_year(self):
+        """
+        Функция получения порядкового номера текущего часа в разрезе года
+        :return:
+        """
+        beginning_of_year = datetime(self.current_datetime.year, 1, 1, tzinfo=self.current_datetime.tzinfo)
+        return (self.current_datetime - beginning_of_year).total_seconds() // 3600
+
+    def get_hourly_forecast(self):
+        """
+        Функция почасового прогноза на основе текущей даты
+        :param datetime:
+        :return:
+        """
+        #Сбор признаков HOY, DOY, Month, Day, Hour (именно в таком порядке)
+        # hoy, doy, month, day, \
+        # hour = np.array([self.hour_of_year(date) for date in pd.date_range(datetime.now(),
+        #                                                                   freq='H', periods=24).round('H')]).T[0:5]
+
+        features = self.X_scaler.transform(np.array([self.hour_of_year(date) for date in pd.date_range(datetime.now(),
+                                                                           freq='H', periods=24).round('H')]))
+        load_forecast = self.Y_scaler.inverse_transform(self.model.predict(features).reshape(len(model.predict(ft)), -1))
+
+        # plt.plot(self.Y_scaler.inverse_transform(self.model.predict(features).reshape(len(model.predict(ft)), -1)))
+
+        return load_forecast
+
+files = ['models/mlp_load_hourly.vrk', 'models/X_scaler_par.sca', 'models/Y_scaler_par.sca']
+L = LoadForecaster(files)
+
+
